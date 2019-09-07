510557adbbbba16d7fa3592dbc03be9ff9cd8c92
Rohan Hasabe <43720061+Hasaber8@users.noreply.github.com>
2019-09-07 18:31:58 +0530

    Derp fix

--- cos_chef.mk
+++ cos_chef.mk
@@ -17,7 +17,7 @@
 # Inherit some common potato stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/cos/config/common_full_phone.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
